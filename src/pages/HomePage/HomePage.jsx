bf6bbf0802a142f7d296067e419f40385ef19fe4
franklpjr <eng.civ.frank@gmail.com>
2023-06-20 15:22:36 -0400

    first commit

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,17 @@
+
+import Navbar from "../../components/Navbar/Navbar";
+import "./HomePage.css"
+
+const HomePage = () => {
+
+
+    return (
+        <div>
+            <Navbar /> {/* Para criar o navbar, rodei o comando npm install styled-components@latest e segui os passos do site https://levelup.gitconnected.com/how-to-create-a-responsive-hamburger-navigation-menu-reactjs-and-styled-components-59ce167ed543 . */}
+            <h1>Novo Site JCD ARMAZÉM</h1>
+        </div>
+    )
+
+}
+
+export default HomePage;
