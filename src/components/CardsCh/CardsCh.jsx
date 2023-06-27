0f31f8d49d411e7d7eb15f596546614943f7ac1a
franklpjr <eng.civ.frank@gmail.com>
2023-06-27 10:50:37 -0400

    componentes de churrasqueiras

--- src/components/CardsCh/CardsCh.jsx
+++ src/components/CardsCh/CardsCh.jsx
@@ -1,60 +1,58 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import "./CardsCh.css"
-import whatsapp from "../../assets/whatsapp-icon.png"
-import palet from "../../assets/palet-icon.png"
-import delivery from "../../assets/delivery-icon.png"
-import years from "../../assets/32-icon.png"
-import  chmedia from "../../assets/chmedia.png"
+import  chmedia from "../../assets/1peixe.png"
+import  chgrande from "../../assets/2peixes.png"
+import  chegrande from "../../assets/3peixes.png"
 
 function CardsCh() {
   return (
    <div className='cards-wrapper'>
-   <Card style={{ width: '14rem' }} className='card-style'>
+    <Card style={{ width: '14rem' }} className='card-style'>
       <Card.Img variant="top" src={chmedia} className='card-icons'/>
       <Card.Body>
-        <Card.Title>Churrasqueira Média</Card.Title>
+        <Card.Title>Média</Card.Title>
         <Card.Text>
-          Altura:<br/>
-          Largura:<br/>
-          Fundo:<br/>
+          Altura: 230cm<br/>
+          Largura: 62cm<br/>
+          Fundo: 56cm<br/>
         </Card.Text>
         {/*<Button variant="primary">Go somewhere</Button>*/}
       </Card.Body>
     </Card>
     
     <Card style={{ width: '14rem' }} className='card-style'>
-      <Card.Img variant="top"  src={delivery} className='card-icons'/>
+      <Card.Img variant="top"  src={chgrande} className='card-icons'/>
       <Card.Body>
-        <Card.Title>Churrasqueira Grande</Card.Title>
+        <Card.Title>Grande</Card.Title>
         <Card.Text>
-          Altura:<br/>
-          Largura:<br/>
-          Fundo:<br/>
+          Altura: 230cm<br/>
+          Largura: 77cm<br/>
+          Fundo: 56cm<br/>
         </Card.Text>
       </Card.Body>
     </Card>
 
     <Card style={{ width: '14rem' }} className='card-style'>
-      <Card.Img variant="top" src={whatsapp} className='card-icons'/>
+      <Card.Img variant="top" src={chegrande} className='card-icons'/>
       <Card.Body>
-        <Card.Title>Churrasqueira Extra Grande</Card.Title>
+        <Card.Title>Extra Grande</Card.Title>
         <Card.Text>
-          Altura:<br/>
-          Largura:<br/>
-          Fundo:<br/>
+          Altura: 230cm<br/>
+          Largura: 77cm<br/>
+          Fundo: 56cm<br/>
         </Card.Text>
       </Card.Body>
     </Card>
 
     <Card style={{ width: '14rem' }} className='card-style'>
-      <Card.Img variant="top" src={palet} className='card-icons' />
+      <Card.Img variant="top" src={chgrande} className='card-icons' />
       <Card.Body>
-        <Card.Title>Churrasqueira Alto Relevo</Card.Title>
+        <Card.Title>Alto Relevo</Card.Title>
         <Card.Text>
-          Altura:<br/>
-          Largura:<br/>
-          Fundo:<br/>
+          Altura: 230cm<br/>
+          Largura: 77cm<br/>
+          Fundo: 56cm<br/>
         </Card.Text>
       </Card.Body>
     </Card>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 import Burger from './Burger';
+import jcdlogo from "../../assets/jcd-log.png";
 
 const Nav = styled.nav`
   width: 100%;
   height: 55px;
-  border-bottom: 2px solid #f1f1f1;
+  color: white;
+  font-style: border;
+  background-color: rgb(255, 81, 28);
   padding: 0 20px;
   display: flex;
   justify-content: space-between;
   .logo {
-    padding: 15px 0;
+    padding: 5px 0;
   }
 `
 
@@ -18,7 +21,7 @@ const Navbar = () => {
   return (
     <Nav>
       <div className="logo">
-        Nav Bar
+      <img src={jcdlogo} width="42px"/>
       </div>
       <Burger />
     </Nav>
